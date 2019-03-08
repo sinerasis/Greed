5f7e86f96cebb2952736a2bb8565d6eff4b0f692
Jeff Baker <jeff.baker@maine.edu>
2019-03-08 12:44:14 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Greed
+## A small extension for Google Chrome / Chromium
+
+Current features of this extension include:
+- Use a simple blank page as the new tab page.
+- Appends site exclusions to Google Search queries automatically.
+- Prevents URLs in Google applications from being routed through Google Search.
+
+To Install and use:
+- Clone this repository.
+- Make any changes you like.
+- Open your browser and navigate to the extension settings (Menu > More tools > Extensions).
+- Enable developer mode.
+- Load unpacked, choose the *directory* you cloned.
+
+The lack of user friendly options is intentional.
+
+My code is not perfect. If  you find a problem or better way of doing something, feel free to submit a pull request.
---
+++ background.js
@@ -0,0 +1,43 @@
+chrome.webRequest.onBeforeRequest.addListener(function(request) {
+    if (request.type == "main_frame") {
+        let originalRequest = new URL(request.url);
+        if (originalRequest.searchParams.has("q")) {
+            let query = originalRequest.searchParams.get("q");
+            
+            // Routing URLs through Google Search is not only an invasion of privacy, it causes non-public URLs to fail.
+            if (originalRequest.pathname == "/url") {
+                console.log("Info: Preventing Google Search bounce.");
+                return { redirectUrl: query };
+            }
+
+            // Google removed the option to hide sources you deem poor, this is gives you that option.
+            let exclude = [
+                "buzzfeed.com",
+                "facebook.com",
+                "foxnews.com",
+                "pintrest.com",
+                "twitter.com",
+                "w3schools.com"
+            ];
+
+            if (exclude.length > 0) {
+                exclude.forEach(function(domain) {
+                    if (query.search(" -site:" + domain) == -1) {
+                        query += " -site:" + domain;
+                    }
+                });
+                if (query != originalRequest.searchParams.get("q")) {
+                    originalRequest.searchParams.delete("q");
+                    originalRequest.searchParams.append("q", query);
+                    console.log("Info: Appending exclusions to search query.");
+                    return { redirectUrl: originalRequest.href };
+                }
+            }
+        }
+    }
+}, {
+    urls: [
+        "http://*.google.com/*",
+        "https://*.google.com/*"
+    ]
+}, [ "blocking"] );
